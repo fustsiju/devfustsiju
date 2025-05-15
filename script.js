86e56dffbadaec6a3d56b9a42a1ed4a9d9d6cdef
fustsiju <fulviost@gmail.com>
2025-05-15 14:26:03 -0300

    Arrumando Links

--- script.js
+++ script.js
@@ -80,5 +80,5 @@ const toggle = document.getElementById('menu-toggle');
 const navLinks = document.getElementById('nav-links');
 
 toggle.addEventListener('click', () => {
-navLinks.classList.toggle('active');
+  navLinks.classList.toggle('active');
 });
